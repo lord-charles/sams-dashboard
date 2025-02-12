df3edc5b8f8f2448f17abc1b2f46c1d85dc6b492
lord-charles <mwanikicharles226@gmail.com>
2025-02-12 10:12:36 +0200

    43

--- app/utils/baseUrl.js
+++ app/utils/baseUrl.js
@@ -1,3 +1,5 @@
-export const base_url = "https://digital-learning.strathmore.edu/express/";
+// export const base_url = "https://digital-learning.strathmore.edu/express/";
+export const base_url = 'https://emis.mogei.gov.ss/express/';
+
 
 // export const base_url = "http://localhost:9000/express/";
